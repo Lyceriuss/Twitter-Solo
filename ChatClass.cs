93d8f01fe4185cd0f524463e4f4dec28e9ecaeb7
Henrik Pedersen <Henrik.Pedersen.vs@gmail.com>
2023-10-26 11:37:35 +0200

    Prototype for ChatFunctionality 1.0

---
+++ ChatClass.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+public class ChatMember
+{
+    public string Username { get; private set; }
+    public string RealName { get; private set; }
+    public int Age { get; private set; }
+    public string MemberID { get; private set; }
+    private string Password { get; set; }           // Private because of reasons
+
+    public ChatMember(string username, string realName, int age, string password)
+    {
+        Username = username;
+        RealName = realName;
+        Age = age;
+        MemberID = GenerateUniqueID(); 
+        Password = password;
+    }
+
+    private string GenerateUniqueID()
+    {
+        
+        return Guid.NewGuid().ToString();   //Might remove this to make sure they are predictable
+    }
+
+    public bool Authenticate(string password)
+    {
+        return Password == password;
+    }
+    public static ChatMember Login(MemberService memberService)
+    {
+
+        Console.WriteLine("Enter username:");
+        string username = Console.ReadLine();
+
+        Console.WriteLine("Enter password:");
+        string password = Console.ReadLine();
+
+        // Validate the user's credentials
+        ChatMember member = memberService.GetMemberByUsername(username);
+        if (member != null && member.Authenticate(password))
+        {
+            Console.WriteLine($"Logged in as {member.Username}.");
+            return member;
+        }
+        else
+        {
+            Console.WriteLine("Invalid username or password.");
+            return null;
+        }
+    }
+}
+
+public class MemberService
+{
+    private List<ChatMember> Members { get; set; } = new List<ChatMember>();
+
+    public ChatMember Register(string username, string realName, int age, string password)
+    {
+        ChatMember newMember = new ChatMember(username, realName, age, password);
+        Members.Add(newMember);
+        return newMember;
+    }
+
+    public ChatMember GetMemberByUsername(string username)
+    {
+        return Members.FirstOrDefault(m => m.Username == username);
+    }
+}
+
+public class MessageService
+{
+    private List<PrivateMessage> Messages { get; set; } = new List<PrivateMessage>();
+
+    public void SendMessage(ChatMember sender, ChatMember receiver, string content)
+    {
+        PrivateMessage newMessage = new PrivateMessage(sender, receiver, content);
+        Messages.Add(newMessage);
+    }
+
+    public List<PrivateMessage> GetMessagesForUser(ChatMember member)
+    {
+        return Messages.Where(m => m.Receiver == member || m.Sender == member).ToList();
+    }
+}
+
+
+public class PrivateMessage
+{
+    public ChatMember Sender { get; private set; }
+    public ChatMember Receiver { get; private set; }
+    public string Content { get; private set; }
+    public DateTime SentTime { get; private set; }
+
+    public PrivateMessage(ChatMember sender, ChatMember receiver, string content)
+    {
+        Sender = sender;
+        Receiver = receiver;
+        Content = content;
+        SentTime = DateTime.Now; // Assigns the current DateTime when the message is created
+    }
+
+    public string DisplayMessage()
+    {
+        return $"{Sender.Username} to {Receiver.Username} at {SentTime:HH:mm}: {Content}";
+    }
+
+ 
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,88 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 
-System.Console.WriteLine("Test");
-System.Console.WriteLine("Test2");
+class Program{
+
+    static void Main(string[] args)
+    {
+        MemberService memberService = new MemberService();
+        MessageService messageService = new MessageService();
+
+        memberService.Register("A", "Abdulina Monaco", 25, "1");
+        memberService.Register("B", "Bibidi Bob", 30, "2");
+        memberService.Register("C", "Chai Tete", 28, "3");
+
+        ChatMember loggedInUser = null;
+
+        while (true)
+        {
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Send Message");
+            Console.WriteLine("3. View Messages");
+            Console.WriteLine("4. Logout");
+            Console.WriteLine("5. Exit");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    loggedInUser = ChatMember.Login(memberService);
+                    break;
+
+                case 2:
+                    if (loggedInUser != null)
+                    {
+                        Console.WriteLine("Enter recipient username:");
+                        string recipientUsername = Console.ReadLine();
+                        ChatMember recipient = memberService.GetMemberByUsername(recipientUsername);
+
+                        if (recipient != null)
+                        {
+                            Console.WriteLine("Enter your message:");
+                            string content = Console.ReadLine();
+                            messageService.SendMessage(loggedInUser, recipient, content);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Recipient not found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You must be logged in to send a message.");
+                    }
+                    break;
+
+                case 3:
+                    if (loggedInUser != null)
+                    {
+                        List<PrivateMessage> userMessages = messageService.GetMessagesForUser(loggedInUser);
+                        foreach (var message in userMessages)
+                        {
+                            Console.WriteLine(message.DisplayMessage());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You must be logged in to view messages.");
+                    }
+                    break;
+
+                case 4:
+                    loggedInUser = null;
+                    Console.WriteLine("Logged out successfully.");
+                    break;
+
+                case 5:
+                    return; 
+
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+    }
+}
